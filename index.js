7e7fd76b2208fae24c8f16daf1b8ff75b4fd44b8
Ankit Biswas <varanasiartist.omg@gmail.com>
2024-02-28 21:30:54 +0530

    second commit

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ import mongoose from "mongoose";
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 //function for establishing connection with mongodb atlas
 function establishMongooseConnection(dbName) {
